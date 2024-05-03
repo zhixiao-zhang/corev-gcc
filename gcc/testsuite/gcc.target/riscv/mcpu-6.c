203302d694758eb583de125938aca4b88f362179
Jeremy Bennett <jeremy.bennett@embecosm.com>
2024-05-03 14:38:07 +0100

    Skip tests which do not invoke a required pass.
    These tests inspect the sched2 pass, which is not used for low
    optimization levels, so we should not run the tests in this case.
    
    	* gcc/testsuite/gcc.target/riscv/mcpu-6.c: Skip the test for -O0,
    	-O1 and -Og.
    	* gcc/testsuite/gcc.target/riscv/mcpu-7.c: Likewise.
    
    Signed-off-by: Jeremy Bennett <jeremy.bennett@embecosm.com>

--- gcc/testsuite/gcc.target/riscv/mcpu-6.c
+++ gcc/testsuite/gcc.target/riscv/mcpu-6.c
@@ -1,6 +1,9 @@
 /* { dg-do compile } */
 /* Verify -mtune has higher priority than -mcpu for pipeline model .  */
 /* { dg-options "-mcpu=sifive-u74 -mtune=rocket -fdump-rtl-sched2-details -march=rv32i -mabi=ilp32" } */
+/* We don't generate have the sched2 pass for low optimization levels:
+   -O0 -O1 and -Og */
+/* { dg-skip-if "" { *-*-* }  { "-O0" "-O1" "-Og" } { "" } } */
 /* { dg-final { scan-rtl-dump "simple_return\[ \]+:alu" "sched2" } } */
 
 int main()
--- gcc/testsuite/gcc.target/riscv/mcpu-7.c
+++ gcc/testsuite/gcc.target/riscv/mcpu-7.c
@@ -1,6 +1,9 @@
 /* { dg-do compile } */
 /* Verify -mtune has higher priority than -mcpu for pipeline model .  */
 /* { dg-options "-mcpu=sifive-s21 -mtune=sifive-u74 -fdump-rtl-sched2-details -march=rv32i -mabi=ilp32" } */
+/* We don't generate have the sched2 pass for low optimization levels:
+   -O0 -O1 and -Og */
+/* { dg-skip-if "" { *-*-* }  { "-O0" "-O1" "-Og" } { "" } } */
 /* { dg-final { scan-rtl-dump "simple_return\[ \]+:sifive_7_B" "sched2" } } */
 
 int main()
